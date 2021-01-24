a86822310eaefc3cea39cb1b6c53c44d62bf12a8
Isaac Ramirez <iramirezc@live.com.mx>
2021-01-23 20:26:43 -0600

    complete effects: - install cross-env - add script for development with mocks - add effect to update breeds on animal change

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,12 +1,28 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useEffect, useState } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
-  const [animal, AnimalDropdown] = useDropdown("Animal", "", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    if (!animal) return;
+
+    pet
+      .breeds(animal)
+      .then(({ breeds: breedsFromApi = [] }) => {
+        const breedsNames = breedsFromApi.map(({ name }) => name);
+
+        setBreeds(breedsNames);
+      })
+      .catch(console.error);
+  }, [animal]);
 
   return (
     <div className="search-params">
