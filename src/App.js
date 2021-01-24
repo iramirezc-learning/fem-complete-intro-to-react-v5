c5671d339829b1a9751b1975cdb257189675a7e0
Isaac Ramirez <iramirezc@live.com.mx>
2021-01-23 20:43:44 -0600

    complete dev tools: - add React StrictMode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me</h1>
-      <SearchParams />
-    </div>
+    <React.StrictMode>
+      <div>
+        <h1>Adopt Me</h1>
+        <SearchParams />
+      </div>
+    </React.StrictMode>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
