77f2833783f0a9df19842e535e6fb9214a892d12
Isaac Ramirez <iramirezc@live.com.mx>
2021-01-24 16:07:56 -0600

    complete class components: - change Details component to be a class component

--- src/Details.js
+++ src/Details.js
@@ -1,9 +1,49 @@
 import React from "react";
+import PetApi from "@frontendmasters/pet";
 
-const Details = (props) => {
-  const { id } = props;
+class Details extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return <h1>{id}</h1>;
-};
+    this.state = {
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    PetApi.animal(this.props.id)
+      .then(({ animal: pet }) => {
+        this.setState({
+          name: pet.name,
+          animal: pet.type,
+          location: `${pet.contact.address.city}, ${pet.contact.address.state}`,
+          description: pet.description,
+          media: pet.photos,
+          breed: pet.breeds.primary,
+          loading: false,
+        });
+      })
+      .catch(console.error);
+  }
+
+  render() {
+    const { loading, name, animal, breed, location, description } = this.state;
+
+    if (loading) {
+      return <h1>Loading...</h1>;
+    }
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Details;
